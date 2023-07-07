701ee1bc069111152a541f521938548727d57c15
Jason Smith <smjason@vmware.com>
2023-07-07 15:43:45 -0400

    Changing  greeting message

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		return "Personal greetings from Spring Boot + Tanzu! Yay!";
 	}
 
 }
